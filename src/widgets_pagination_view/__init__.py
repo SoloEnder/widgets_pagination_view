47b442b4d50c7c1576ecbf7de884d1d8424a42f8
SoloEnder <houngbedjileonide@protonmail.com>
2026-06-02 22:19:18 +0200

    pushed the whole code

---
+++ src/widgets_pagination_view/__init__.py
@@ -0,0 +1,5 @@
+
+from .widgets_pagination_view import WidgetsPaginationView, InPageWidget
+from .exceptions import InvalidWidgetIndexError, PageNotLoadedError
+
+__all__ = ["WidgetsPaginationView", "InPageWidget", "InvalidWidgetIndexError", "PageNotLoadedError"]
